f0d72c3583a693e6cc82e9150209e7e70ff55d06
DIGIX666 <digixit66@gmail.com>
2022-07-04 16:54:48 +0200

    k

--- main.go
+++ main.go
@@ -4,43 +4,20 @@ import (
 	"fmt"
 	"io/ioutil"
 	"math"
-	"sort"
 	"strconv"
 	"strings"
 )
 
-func main() {
-	file, err := ioutil.ReadFile("data.txt")
-	if err != nil {
-		fmt.Println(err)
-	}
-	var numbers []float64
-	lines := strings.Split(string(file), "\n")
-	for _, line := range lines {
-		if line == "" {
-			continue
-		}
-		num, _ := strconv.ParseFloat(line, 64)
-		numbers = append(numbers, num)
-	}
-
-	// if \
-
-	sort.Float64s(numbers)
-	fmt.Println("Average :", math.Round((average(numbers))))
-	fmt.Println("Median :", math.Round(median(numbers)))
-	fmt.Println("Variance :", math.Round(variance(numbers)))
-	fmt.Println("Standard deviation :", math.Round(standardDeviation(numbers)))
-}
-
+// calcul Moyen
 func average(numbers []float64) float64 {
-	var sum float64
+	var somme float64
 	for _, number := range numbers {
-		sum += number
+		somme += number
 	}
-	return sum / float64(len(numbers))
+	return somme / float64(len(numbers))
 }
 
+//calcul Median
 func median(numbers []float64) float64 {
 	var median float64
 	if len(numbers)%2 == 0 {
@@ -51,6 +28,8 @@ func median(numbers []float64) float64 {
 	return median
 }
 
+
+//calcul Variance 
 func variance(numbers []float64) float64 {
 	mean := average(numbers)
 	var variance float64
@@ -60,6 +39,33 @@ func variance(numbers []float64) float64 {
 	return variance / float64(len(numbers))
 }
 
+
+//calcul Ecart-types
 func standardDeviation(numbers []float64) float64 {
 	return math.Sqrt(variance(numbers))
+}
+
+
+
+func main() {
+	file, err := ioutil.ReadFile("data.txt")
+	if err != nil {
+		fmt.Println(err)
+	}
+	var numbers []float64
+	lines := strings.Split(string(file), "\n") // Pour décomposer les informations dans "file" en une liste de sous-chaîne en séparant les informations par des retour à la ligne 
+	for _, line := range lines {
+		if line == "" {
+			continue
+		}
+		num, _ := strconv.ParseFloat(line, 64)
+		numbers = append(numbers, num)
+	}
+
+	
+	// sort.Float64s(numbers)
+	fmt.Println("Average :", math.Round((average(numbers))))
+	fmt.Println("Median :", math.Round(median(numbers)))
+	fmt.Println("Variance :", math.Round(variance(numbers)))
+	fmt.Println("Standard deviation :", math.Round(standardDeviation(numbers)))
 }
