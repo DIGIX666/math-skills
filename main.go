7c4c8de2b9d40e3ecace6adcea0ae49712351a01
DIGIX666 <digixit66@gmail.com>
2022-07-04 17:53:21 +0200

    k

--- main.go
+++ main.go
@@ -67,6 +67,6 @@ func main() {
 	sort.Float64s(numbers)
 	fmt.Println("Average :", math.Round((average(numbers))))
 	fmt.Println("Median :", math.Round(median(numbers)))
-	fmt.Println("Variance :", int(variance(numbers)))
-	fmt.Println("Standard deviation :", math.Round(standardDeviation(numbers)))
+	fmt.Println("Variance :", int(math.Round(variance(numbers))))
+	fmt.Println("Standard deviation :", int(math.Round(standardDeviation(numbers))))
 }
